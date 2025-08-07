fd49e046873a6c6f9acbaef223c9add648d3b7d9
anshut0208 <154457143+anshut0208@users.noreply.github.com>
2025-08-08 03:45:22 +0530

    Delete src directory

--- src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import './Footer.css';
-export default function Footer(){
-    return(
-        <div className="footer">
-            <p>Copyright &copy; 2025 || JARP all rights reserved   </p>
-        </div>
-    )
-}
